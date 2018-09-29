a13ab7ad990a64a8a520ea2de9d48d03595ff1c4
liu-bill <42587821+liu-bill@users.noreply.github.com>
2018-09-29 11:50:40 -0400

    Transfer aiming script to Jamie

---
+++ RotateWithMouse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithMouse : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.LookRotation (
+			Vector3.forward, Camera.main.ScreenToWorldPoint (Input.mousePosition)-transform.position);
+		if (Input.GetMouseButtonDown (0))
+			rb.velocity = transform.up;
+	}
+}
